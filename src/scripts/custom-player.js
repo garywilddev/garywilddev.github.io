05e254cf08bd243381d5be07a4f7a127581cbde5
Gary Sauvage <garywilddev@gmail.com>
2024-03-19 13:01:59 +0100

    add controls for hero video

---
+++ src/scripts/custom-player.js
@@ -0,0 +1,25 @@
+$(() => {
+  $(document).ready(function () {
+    const media = $("#hero-video");
+    const controls = $("#hero-player .controls");
+
+    const play = $("#play");
+    const stop = $("#stop");
+    media.removeAttr("controls");
+    controls.css("visibility", "visible");
+
+    play.bind("click", playPauseMedia);
+
+    function playPauseMedia() {
+      $("#play").each((_, button) => {
+        $(button).toggleClass("fa-circle-play");
+        $(button).toggleClass("fa-circle-pause");
+      });
+      if (media[0].paused) {
+        media[0].play();
+      } else {
+        media[0].pause();
+      }
+    }
+  });
+});
