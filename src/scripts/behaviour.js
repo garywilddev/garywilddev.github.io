7e026af80089a05739801c6ea6e2421192aa6353
Gary Sauvage <garywilddev@gmail.com>
2024-03-21 11:08:29 +0100

    improve hero video resize

--- src/scripts/behaviour.js
+++ src/scripts/behaviour.js
@@ -1,4 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
+function selectCurrentSection() {
+  /* BEGIN SELECT-CURRENT-SECTION */
+  // select current section based on intersection when scrolling
   const state = {
     _value: "",
     _listener: function (val) {},
@@ -44,56 +46,93 @@ document.addEventListener("DOMContentLoaded", () => {
   targetSections.forEach((section) => {
     observer.observe(section);
   });
-});
+  /* END SELECT-CURRENT-SECTION */
+}
+
+function resizeHero() {
+  /* BEGIN RESIZE-HERO */
+  const header = $("header").first();
+  const player = $("#hero-player");
+  const hero = $("#hero");
+  player.height(`calc(100vh - ${header.height()}px)`);
+  hero.css("min-height", `calc(100vh - ${header.height()}px)`);
+  hero.height(`calc(100vh - ${header.height()}px)`);
+  /* END RESIZE-HERO */
+}
+
+function translateMain() {
+  /* BEGIN TRANSLATE-MAIN */
+  const header = $("header").first();
+  const main = $("main").first();
+
+  main.css("transform", `translate(0, ${header.height()}px)`);
+  /* END TRANSLATE-MAIN */
+}
+
+function toTop() {
+  /* BEGIN TO-THE-TOP */
+  //Click Logo To Scroll To Top
+  $("#logo").on("click", () => {
+    $("html,body").animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    );
+  });
+
+  // Behaviour of To-Top button
+  $("#to-top").on("click", (e) => {
+    $("html,body").animate({ scrollTop: 0 }, 500);
+  });
+
+  //Show To-Top button when scrolling
+  $(window).on("scroll", (e) => {
+    const navBarYPostion = $("header").position().top;
+    const element = $("#to-top");
+
+    if (!element.hasClass("show") && navBarYPostion > 0) {
+      element.addClass("show");
+    } else if (navBarYPostion === 0) {
+      element.removeClass("show");
+    }
+  });
+  /* END TO-THE-TOP */
+}
+
+function navBar() {
+  /* BEGIN NAV-BAR */
+  //Toggle Menu
+  $(".burger").on("click", function () {
+    $(this).toggleClass("is-open");
+    $("#main-nav").toggleClass("is-open");
+    $("li").on("click", (e) => {
+      $("#main-nav").removeClass("is-open");
+      $(".burger").removeClass("is-open");
+    });
+  });
+
+  // Smooth scrolling for Toggle Menu Items
+  $('a[href^="#"').each((_, a) => {
+    $(a).on("click", (e) => {
+      e.preventDefault();
+      const navContainerHeight = $("#header-content").outerHeight(
+        true /*include margin*/
+      );
+      const top = $(`${$(a).attr("href")}`).offset().top;
+      const position = top - navContainerHeight;
+      $("html,body").animate({ scrollTop: position }, 500);
+    });
+  });
+  /* END NAV-BAR */
+}
 
 $(() => {
   $(document).ready(function () {
-    //Click Logo To Scroll To Top
-    $("#logo").on("click", () => {
-      $("html,body").animate(
-        {
-          scrollTop: 0,
-        },
-        500
-      );
-    });
-
-    $("#to-top").on("click", (e) => {
-      $("html,body").animate({ scrollTop: 0 }, 500);
-    });
-
-    $(window).on("scroll", (e) => {
-      const navBarYPostion = $("header").position().top;
-      const element = $("#to-top");
-
-      if (!element.hasClass("show") && navBarYPostion > 0) {
-        element.addClass("show");
-      } else if (navBarYPostion === 0) {
-        element.removeClass("show");
-      }
-    });
-
-    //Smooth scrolling for Toggle Menu Items
-    $('a[href^="#"').each((_, a) => {
-      $(a).on("click", (e) => {
-        e.preventDefault();
-        const navContainerHeight = $("#header-content").outerHeight(
-          true /*include margin*/
-        );
-        const top = $(`${$(a).attr("href")}`).offset().top;
-        const position = top - navContainerHeight;
-        $("html,body").animate({ scrollTop: position }, 500);
-      });
-    });
-
-    //Toggle Menu
-    $(".burger").on("click", function () {
-      $(this).toggleClass("is-open");
-      $("#main-nav").toggleClass("is-open");
-      $("li").on("click", (e) => {
-        $("#main-nav").removeClass("is-open");
-        $(".burger").removeClass("is-open");
-      });
-    });
+    selectCurrentSection();
+    translateMain();
+    toTop();
+    navBar();
+    resizeHero();
   });
 });
--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,22 +1,60 @@
 $(() => {
-  style = ": ";
   $(document).ready(function () {
+    const DELTA = 0;
+    const MOVING_FACTOR = 0.5;
+    const SCALE_FACTOR = 0.1;
+
     const player = $("#hero-player");
     const controls = $("#hero-player .controls");
     const container = $("#hero-container");
 
-    $(window).on("scroll", (e) => {
-      const value = window.scrollY;
-      const scaleFactor = value * 0.05;
-      const tx = ((1 * scaleFactor) / 100) * player.width();
-      const ty = ((-1 * scaleFactor) / 100) * player.height();
+    const startEffectYPosition = 0; // start effect at the top of the page
+    const endEffectYPosition = 14 / SCALE_FACTOR; // end effect when inset scale reaches 14%
+
+    function movePlayer(value) {
+      const tx = 0;
+      const ty = MOVING_FACTOR * value;
+      player.css("transform", `matrix(1, 0, 0, 1, ${tx}, ${ty})`);
+    }
+
+    function cropPlayer(value) {
+      const scale = value * SCALE_FACTOR;
+      const tx = ((1 * scale) / 100) * player.width();
+      const ty = ((-1 * scale) / 100) * player.height();
 
       controls.css("transform", `matrix(1, 0, 0, 1, ${tx}, ${ty})`);
 
-      if (value > 0) {
-        player.css("clip-path", `inset(${scaleFactor}% round 8.62200px`);
+      if (value > startEffectYPosition) {
+        player.css("clip-path", `inset(${scale}% round ${value * 0.09}px`);
       } else {
         player.css("clip-path", "");
+      }
+    }
+
+    let lastScrollTop = 0;
+    $(window).on("scroll", (e) => {
+      //translate controls as the window is scrolled
+      const value = window.scrollY;
+
+      var nowScrollTop = $(this).scrollTop();
+
+      if (Math.abs(lastScrollTop - nowScrollTop) >= DELTA) {
+        if (nowScrollTop > lastScrollTop) {
+          // ACTION ON
+          // SCROLLING DOWN
+          if (value >= startEffectYPosition && value <= endEffectYPosition) {
+            movePlayer(value);
+            cropPlayer(value);
+          }
+        } else {
+          // ACTION ON
+          // SCROLLING UP
+          if (value >= startEffectYPosition && value <= endEffectYPosition) {
+            movePlayer(value);
+            cropPlayer(value);
+          }
+        }
+        lastScrollTop = nowScrollTop;
       }
     });
   });
