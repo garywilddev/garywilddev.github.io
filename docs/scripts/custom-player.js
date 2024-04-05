c33de0a9e3193ea1ee28f21600c21c4284f63b9f
Gary Sauvage <garywilddev@gmail.com>
2024-04-05 17:00:34 +0200

    remove reundant document.ready jquery handler

--- docs/scripts/custom-player.js
+++ docs/scripts/custom-player.js
@@ -1,24 +1,22 @@
 $(() => {
-  $(document).ready(function () {
-    const media = $("#hero-video");
-    const controls = $("#hero-player .controls");
+  const media = $("#hero-video");
+  const controls = $("#hero-player .controls");
 
-    const play = $("#play");
-    media.removeAttr("controls");
-    controls.css("visibility", "visible");
+  const play = $("#play");
+  media.removeAttr("controls");
+  controls.css("visibility", "visible");
 
-    play.bind("click", playPauseMedia);
+  play.bind("click", playPauseMedia);
 
-    function playPauseMedia() {
-      $("#play").each((_, button) => {
-        $(button).toggleClass("fa-circle-play");
-        $(button).toggleClass("fa-circle-pause");
-      });
-      if (media[0].paused) {
-        media[0].play();
-      } else {
-        media[0].pause();
-      }
+  function playPauseMedia() {
+    $("#play").each((_, button) => {
+      $(button).toggleClass("fa-circle-play");
+      $(button).toggleClass("fa-circle-pause");
+    });
+    if (media[0].paused) {
+      media[0].play();
+    } else {
+      media[0].pause();
     }
-  });
+  }
 });
